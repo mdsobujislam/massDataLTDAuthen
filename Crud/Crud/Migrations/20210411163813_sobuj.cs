e8e64bfa1bda7c8151119ab48fd8fdc2c26fcfc4
mdsobujislam <sobujislam674@gmail.com>
2021-04-11 23:09:53 +0600

    commit

---
+++ Crud/Crud/Migrations/20210411163813_sobuj.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Crud.Migrations
+{
+    public partial class sobuj : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "products",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ProductName = table.Column<string>(maxLength: 40, nullable: false),
+                    ProductPrice = table.Column<string>(maxLength: 40, nullable: false),
+                    ProductQuantity = table.Column<string>(maxLength: 40, nullable: false),
+                    ProductBrand = table.Column<string>(maxLength: 40, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_products", x => x.ProductId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "products");
+        }
+    }
+}
---
+++ Crud/Crud/Models/DbModels.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Crud.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        [Required, StringLength(40), Display(Name = "Product Name")]
+        public string ProductName { get; set; }
+        [Required, StringLength(40), Display(Name = "Product Price")]
+        public string ProductPrice { get; set; }
+        [Required, StringLength(40), Display(Name = "Product Quantity")]
+        public string ProductQuantity { get; set; }
+        [Required, StringLength(40), Display(Name = "Product Brand")]
+        public string ProductBrand { get; set; }
+    }
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext(DbContextOptions<ProductDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Product> products { get; set; }
+    }
+}
